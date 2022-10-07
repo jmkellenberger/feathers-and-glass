413f2ac5c5b442eeedbb24e091fd1d223ce687eb
Jack Kellenberger <jackson@kellenberger.io>
2022-10-06 22:02:47 -0400

    Basic movement and generic rendering

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,83 @@
 #[warn(clippy::pedantic)]
+mod components;
+mod map;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
+    pub use specs::prelude::*;
+    pub use specs_derive::Component;
+    pub use std::cmp::{max, min};
     pub const _SCREEN_WIDTH: i32 = 80;
     pub const _SCREEN_HEIGHT: i32 = 50;
     pub const _MAPSIZE: i32 = _SCREEN_HEIGHT * _SCREEN_WIDTH;
+    pub use crate::components::*;
+    pub use crate::map::*;
 }
 
 use prelude::*;
 
-struct State {}
+struct LeftWalker {}
+
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+
+    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 {
+                pos.x = 79
+            }
+        }
+    }
+}
+
+struct State {
+    ecs: World,
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftWalker {};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(1, 1, "Hello Nihn");
+
+        self.run_systems();
+        player_input(self, ctx);
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.color.fg, render.color.bg, render.glyph);
+        }
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut BTerm) {
+    match ctx.key {
+        None => {}
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        },
+    }
+}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
     }
 }
 
@@ -22,6 +85,32 @@ fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Feathers and Glass")
         .build()?;
-    let gs = State {};
+    let mut gs = State { ecs: World::new() };
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
+
+    gs.ecs
+        .create_entity()
+        .with(Player {})
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: to_cp437('@'),
+            color: ColorPair::new(YELLOW, BLACK),
+        })
+        .build();
+
+    for i in 0..10 {
+        gs.ecs
+            .create_entity()
+            .with(Position { x: i * 7, y: 20 })
+            .with(Renderable {
+                glyph: to_cp437('☺'),
+                color: ColorPair::new(RED, BLACK),
+            })
+            .with(LeftMover {})
+            .build();
+    }
     main_loop(context, gs)
 }
